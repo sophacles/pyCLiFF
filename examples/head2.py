1056a20fcd206fad0eab13ef68e45bfe58e117d6
Erich Heine <sophacles@gmail.com>
2009-08-20 13:35:00 -0500

    Added an alternate method of the head script, as an example of avoiding globals (for not so quick and dirty filters without subclassing CLF)

---
+++ examples/head2.py
@@ -0,0 +1,33 @@
+from pyCLiFF import CLF
+
+class head(object):
+    def __init__(self, *kw):
+        self.lineno = 0
+        self.maxline = kw.get('max', 10)
+        self.fmt = kw.get('fmt', "%(line)s")
+        self.myfilter = CLF(options= [(('-n',),{'type':'int', 'default':10}),
+                                (('-L', '--number-lines'), {'dest':'num', 'action':'store_true'})],
+                    handle_opts = self.handle_opts, processdata=self.datahandler)
+
+
+    def handle_opts(self, opts):
+        self.maxline = opts.n
+        if opts.num:
+            self.fmt = "%(lineno)s: %(line)s"
+
+    def datahandler(line):
+        if self.lineno > self.maxline:
+            raise StopIteration
+
+        self.lineno += 1
+        vals = self.__dict__.copy()
+        vals.update(locals())
+        return fmt % vals
+
+    def run(self):
+        self.myfilter.main()
+
+
+if __name__ == '__main__':
+    x = head()
+    x.run()
